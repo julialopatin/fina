8bd91ca1907a540f2e92294f80d8975d752e96b1
julial <julia_lopatin@hotmail.com>
2023-08-18 00:21:49 +0300

    17.8.2023

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 
 function App() {
-  return (
-    <div homepage>
-    </div>
-  );
-}
+    <Header/>
+    <Nav/>
+    <Hero/>
+    <Highlites/>
+    <Testimonials/>
+    <About/>
+    <Footer/>
+};
 
 export default App;
---
+++ src/nav.js
@@ -0,0 +1,15 @@
+function Menybar {
+    return (
+        <><div classname="little lemon">
+            <img src={require('lemon.png')} alt='little lemon logo'></img>
+        </div>
+        <ul>
+                <li><a href="home">home</a></li>
+                <li><a href="about">about</a></li>
+                <li><a href="meny">meny</a></li>
+                <li><a href="reservation">reservation</a></li>
+                <li><a href="order online">order online</a></li>
+                <li><a href="sign in">sign in</a></li>
+            </ul></>
+         )
+};
